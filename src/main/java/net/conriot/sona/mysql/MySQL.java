31c903ac864f2857920dc82b067e9d202fcf6eb1
Endain <endain.gaming@gmail.com>
2013-10-18 00:18:53 -0700

    Initial commit, now using Maven

---
+++ src/main/java/net/conriot/sona/mysql/MySQL.java
@@ -0,0 +1,18 @@
+package net.conriot.sona.mysql;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MySQL extends JavaPlugin 
+{
+	@Override
+	public void onEnable()
+	{
+		// TODO
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		// TODO
+	}
+}
